91d6bf0492ba7f95a4ff806275954257a6908483
Tobias Brunner <tobias@tobru.ch>
2015-12-04 08:36:09 +0100

    very simple initial spec tests

---
+++ spec/classes/init_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+describe 'burp' do
+  context 'supported operating systems' do
+    ['Debian', 'RedHat'].each do |osfamily|
+      describe "burp class without any parameters on #{osfamily}" do
+        let(:params) {{ }}
+        let(:facts) {{
+          :osfamily => osfamily,
+        }}
+
+        it { is_expected.to compile.with_all_deps }
+
+        it { is_expected.to contain_class('burp::install').that_comes_before('burp::config') }
+        it { is_expected.to contain_class('burp::config') }
+        it { is_expected.to contain_class('burp') }
+
+        it { is_expected.to contain_file('/etc/burp').with_ensure('directory') }
+        it { is_expected.to contain_package('burp').with_ensure('installed') }
+      end
+    end
+  end
+end
---
+++ spec/classes/server_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+describe 'burp::server' do
+  context 'supported operating systems' do
+    ['Debian', 'RedHat'].each do |osfamily|
+      describe "server class without any parameters on #{osfamily}" do
+        let(:params) {{ }}
+        let(:facts) {{
+          :osfamily => osfamily,
+        }}
+
+        it { is_expected.to compile.with_all_deps }
+        it { is_expected.to contain_class('burp') }
+        it { is_expected.to contain_class('burp::server') }
+        it { is_expected.to contain_user('burp').with_ensure('present') }
+        it { is_expected.to contain_file('/etc/burp/burp-server.conf').with_ensure('file') }
+        it { is_expected.to contain_file('/etc/burp/CA.cnf').with_ensure('file') }
+        it { is_expected.to contain_file('/etc/burp/clients').with_ensure('directory') }
+        it { is_expected.to contain_file('/etc/rsyslog.d/21-burp.conf').with_ensure('file') }
+        it { is_expected.to contain_file('/usr/local/bin/burp_notify_script').with_ensure('file') }
+        it { is_expected.to contain_file('/usr/local/bin/burp_timer_script').with_ensure('file') }
+        it { is_expected.to contain_file('/usr/local/bin/burp_summary_script').with_ensure('file') }
+        it { is_expected.to contain_file('/usr/local/bin/burp_ssl_extra_checks_script').with_ensure('file') }
+        it { is_expected.to contain_file('/var/lib/burp').with_ensure('directory') }
+        it { is_expected.to contain_augeas('/etc/default/burp') }
+        it { is_expected.to contain_service('burp').with_ensure('running') }
+
+      end
+    end
+  end
+end
